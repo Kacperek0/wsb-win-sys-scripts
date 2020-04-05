a76ba877506e2dbce7298f606e1eebf412fd13e6
Kacper Szczepanek <kacper.szczepanek@nordcloud.com>
2020-04-05 15:05:56 +0200

    create copy script

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 Scripts created for academical purposes.
+
+* tworz_katalogi.bat
+    * Script creating folders tree and downloading pics using AWS S3 and unziping with Powershell.
+* kopia.bat
+    * Script which is creating copy and moving files to copy from selected source.
---
+++ lab3/kopia.bat
@@ -0,0 +1,27 @@
+@echo off
+
+    REM check if directory exists, remove it and create again
+    if exist "kopia" (
+        rmdir /q /s "kopia"
+        mkdir "kopia"
+    )
+
+    REM delete existing downloads file
+    if exist "download" (
+        rmdir /q /s "download"
+    )
+
+    REM create and fill in new downloads folder
+    mkdir "download"
+    powershell Invoke-WebRequest https://wsb-architektura-komputerow.s3.eu-central-1.amazonaws.com/winlab.zip -O winlab.zip
+    powershell Expand-Archive winlab.zip
+    del /f "winlab.zip"
+    for /f %%f in ('dir /s /b .\winlab\download') do (
+        move %%f .\download
+    )
+    rmdir /q /s "winlab"
+
+    REM move files from download folder to kopia
+    for /f %%f in ('dir /s /b .\download') do (
+        copy %%f .\kopia
+    )
